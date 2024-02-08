ce37c2554775489573f90bcc78cb57be7ec7da24
Tawsif Torabi <tawsiftorabi.jr@gmail.com>
2024-02-08 18:05:54 +0600

    Added FAIL Safe

--- Sci_Arm/Sabretooth_Drive/Arm_Actuator_Control.ino
+++ Sci_Arm/Sabretooth_Drive/Arm_Actuator_Control.ino
@@ -1,12 +1,26 @@
+// Software Serial Sample for Packet Serial
+// Copyright (c) 2012 Dimension Engineering LLC
+// See license.txt for license details.
 #include <PS4Controller.h>
 #include <SoftwareSerial.h>
 #include <Sabertooth.h>
 
+
 #define WRIST_X_PIN 14
 #define WRIST_Z_PIN 12
 
+//C8:F0:9E:7A:C3:06 - Torabi ESP32 Mac 
+
+
+// Up to 8 Sabertooth/SyRen motor drivers can share the same S1 line.
+// This sample uses three: address 128 and 129 on ST1[0] and ST1[2],
+// and address 130 on ST2.
 SoftwareSerial SWSerial(NOT_A_PIN, 13);
 Sabertooth ST1[2] = { Sabertooth(130, SWSerial), Sabertooth(129, SWSerial) };
+
+
+long lastTimeStamp;
+
 
 void onConnect(){
   Serial.println("Connected!.");
@@ -17,78 +31,49 @@ void onDisConnect(){
   Serial.println("Disconnected!.");    
 }
 
-void setup(){
-  SWSerial.begin(9600);
-  Sabertooth::autobaud(SWSerial);
-  Serial.begin(115200);
-  
-  PS4.attachOnConnect(onConnect);
-  PS4.attachOnDisconnect(onDisConnect);
-  PS4.begin();
-
-  pinMode(WRIST_X_PIN, OUTPUT);
-  pinMode(WRIST_Z_PIN, OUTPUT);
-
-  Serial.println("Ready.");
+void Failsafe(){
+  digitalWrite(WRIST_X_PIN, LOW);
+  digitalWrite(WRIST_Z_PIN, LOW);
+  Gripper(0, 0);
+  Gripper(0, 1);
+  Actuator(0, 0);
+  Actuator(0, 1);
 }
 
-void loop(){
-  int RightY = applyDeadzone(PS4.RStickY(), 20);
-  int RightX = applyDeadzone(PS4.RStickX(), 20);
-  int LeftY = applyDeadzone(PS4.LStickY(), 20);
-
-  // Actuator 1
-  if(RightY != 0){
-    Actuator(RightY, 0);
-    Serial.print("RY: ");
-    Serial.println(RightY);
-  } else {
-    Actuator(0, 0);
+// Function to apply deadzone logic
+int applyDeadzone(int value, int deadzone) {
+  if (value > deadzone || value < -deadzone) {
+    // Map the value to the range of 127 to -127
+    return map(value, -127, 127, -127, 127);
+    //return value;
   }
-
-  // Actuator 2
-  if(RightX != 0){
-    Actuator(RightX, 1);
-    Serial.print("RX: ");
-    Serial.println(RightX);
-  } else {
-    Actuator(0, 1);
-  }
-  
-  // Gripper
-  if(PS4.L1()){
-    Gripper(80, 0);
-    Serial.println("Grip Close");
-  } else if(PS4.R1() == 1){
-    Gripper(-80, 0);
-    Serial.println("Grip Open");
-  } else {
-    Gripper(0, 0);
-  }
-
-  // Wrist Joint
-  if(PS4.Up()){
-    digitalWrite(WRIST_X_PIN, HIGH);
-    Serial.println("WristX UP");
-  } else if(PS4.Down()){
-    digitalWrite(WRIST_X_PIN, LOW);
-    Serial.println("WristX DOWN");
-  } else if(PS4.Left()){
-    digitalWrite(WRIST_Z_PIN, HIGH);
-    Serial.println("WristZ UP");
-  } else if(PS4.Right()){
-    digitalWrite(WRIST_Z_PIN, LOW);
-    Serial.println("WristZ DOWN");
-  } else {
-    digitalWrite(WRIST_X_PIN, LOW);
-    digitalWrite(WRIST_Z_PIN, LOW);
-  }
-
-  // Joint Failsafe
-  if (!PS4.Up() && !PS4.Down() && !PS4.Left() && !PS4.Right()) {
-    jointFailsafe();
-  }
+  return 0;  // Return 0 if within the deadzone
 }
+
+void responseToController(){
+  
+  // PS4.LStickX(),
+  // PS4.LStickY(),
+  // PS4.RStickX(),
+  // PS4.RStickY(),
+  // PS4.Left(),
+  // PS4.Down(),
+  // PS4.Right(),
+  // PS4.Up(),
+  // PS4.Square(),
+  // PS4.Cross(),
+  // PS4.Circle(),
+  // PS4.Triangle(),
+  // PS4.L1(),
+  // PS4.R1(),
+  // PS4.L2(),
+  // PS4.R2(),  
+  // PS4.Share(),
+  // PS4.Options(),
+  // PS4.PSButton(),
+  // PS4.Touchpad(),
+}
+
 
 void Actuator(int speed, int Actuator){
   if(Actuator == 0){
@@ -104,18 +89,97 @@ void Gripper(int speed, int Gripper){
      ST1[0].motor(1, speed);
   }
   if(Gripper == 1){
-    ST1[1].motor(2, speed);
+    ST1[0].motor(2, speed);
   }
 }
 
-void jointFailsafe(){
-  digitalWrite(WRIST_X_PIN, LOW);
-  digitalWrite(WRIST_Z_PIN, LOW);
+void setup(){
+  //Initialize Software Serial with Sabretooth AutoBaud
+  SWSerial.begin(9600);
+  Sabertooth::autobaud(SWSerial); // Autobaud is for the whole serial line -- you don't need to do
+                                  // it for each individual motor driver. This is the version of
+                                  // the autobaud command that is not tied to a particular
+                                  // Sabertooth object.
+                                  // See the examples in 1.Basics for information on whether you
+                                  // need this line at all.
+
+  //Initialize USB Serial
+  Serial.begin(115200);
+
+  //Attach Functions to Event Listeners
+  //PS4.attach(responseToController);
+  PS4.attachOnConnect(onConnect);
+  PS4.attachOnDisconnect(onDisConnect);
+  
+  //Initialize PS4 Controller Master
+  PS4.begin();
+
+  Serial.println("Ready.");
+
+  //Reset To Default State
+  Failsafe();
 }
 
-int applyDeadzone(int value, int deadzone) {
-  if (value > deadzone || value < -deadzone) {
-    return map(value, -127, 127, -127, 127);
+
+
+void loop(){
+  if (Serial.available() > 0) {
+    String str = Serial.readStringUntil('\n'); // Read the string until newline character
+    if (str.equals("batlvl")) { // Compare strings using equals() method
+      Serial.printf("Battery Level : %d\n", PS4.Battery());
+      Serial.println();
+    }
   }
-  return 0;  // Return 0 if within the deadzone
+
+  int RightY = PS4.RStickY();
+  int RightX = PS4.RStickX();
+  int LeftY = PS4.LStickY();
+
+
+  //actuator
+  if(applyDeadzone(RightY, 20) != 0){
+    Actuator(RightY, 0);
+    Serial.print("RY: ");
+    Serial.println(RightY);
+  }else{
+    Actuator(0, 0);
+  }
+
+  if(applyDeadzone(RightX, 20) != 0){
+    Actuator(RightX, 1);
+    Serial.print("RX: ");
+    Serial.println(RightX);
+  }else{
+    Actuator(0, 1);
+  }
+  
+
+  //gripper
+  if(PS4.L1()){
+    Gripper(-127, 0);
+    Gripper(-127, 1);
+    //Serial.println("Grip Close");
+  }
+  else{
+    Gripper(0, 0);
+    Gripper(0, 1);
+  }
+  
+  if(PS4.R1()){
+    Gripper(127, 0);
+    Gripper(127, 1);
+    //Serial.println("Grip Open");
+  }
+  else{
+    Gripper(0, 0);
+    Gripper(0, 1);
+  }
+
+
+
+  //Only needed to print the message properly on serial monitor. Else we don't need it.
+  if (millis() - lastTimeStamp > 50) {
+    lastTimeStamp = millis();
+  }
 }
+
